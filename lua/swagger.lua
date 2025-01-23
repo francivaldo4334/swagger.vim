8d9612249270dc88213c902fd044a5c97304fd3f
francivaldo4334 <francivaldodev@gmail.com>
2025-01-23 15:52:19 -0300

    save

--- lua/swagger.lua
+++ lua/swagger.lua
@@ -1,26 +1,20 @@
 local M = {}
+
 function M.openSwaggerUi(swaggerurl)
-	print("hello world. you url is " .. swaggerurl)
+	if swaggerurl == "" then
+		print("Error: URL cannot be empty.")
+	else
+		print("Opening Swagger UI with URL: " .. swaggerurl)
+	end
 end
 
-function M.setup(opts)
-	local group = vim.api.nvim_create_augroup("swagger", { clear = true })
-	vim.api.nvim_create_user_command("SwaggerUi", function(opts)
-		local swaggerurl = opts.args
+function M.setup()
+	vim.api.nvim_create_user_command("SwaggerUi", function(event)
+		local swaggerurl = event.args
 		M.openSwaggerUi(swaggerurl)
 	end, {
 		nargs = 1,
 		desc = "Define a url do swagger que será utilizada.",
-		complete = "swagger",
-	})
-	vim.api.nvim_create_autocmd("AddSwaggerUrl", {
-		group = group,
-		pattern = "swagger_pat",
-		callback = function(event)
-			---@type number
-			local buf = event.buf == 0 and vim.api.nvim_get_current_buf() or event.buf
-			print("hello world")
-		end,
 	})
 end
 
