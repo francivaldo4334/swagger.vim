f77c43bdb465183bdbc4d573ff9eeb124c0c9361
francivaldo4334 <francivaldodev@gmail.com>
2025-01-23 15:27:47 -0300

    save

--- lua/swagger.lua
+++ lua/swagger.lua
@@ -1,5 +1,17 @@
-return {
-  setup = function(opts)
-    print("hello world")
-  end,
-}
+local M = {}
+function M.openSwaggerUi(swaggerurl)
+	print("hello world. you url is " .. swaggerurl)
+end
+
+function M.setup(opts)
+	vim.api.nvim_create_user_command("SwaggerUi", function(opts)
+		local swaggerurl = opts.args
+		M.openSwaggerUi(swaggerurl)
+	end, {
+		nargs = 1,
+		desc = "Define a url do swagger que será utilizada.",
+		complete = "swagger",
+	})
+end
+
+return M
